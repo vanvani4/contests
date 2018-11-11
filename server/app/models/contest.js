5b639fe9ef45e678574260727accd95be16c855a
vanvani4 <iiiordanov@gmail.com>
2018-11-11 23:15:36 +0200

    v0.1.1

--- server/app/models/contest.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Contest', {
-    id: Number,
-    name: String,
-    image: String,
-    description: String
-});
--- server/config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'mongodb://admin:admin512800@ds241121.mlab.com:41121/contests'
-};
